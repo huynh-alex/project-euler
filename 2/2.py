906a095592d5ceeb51bea802d762ed1265836d9a
unknown <alexhuynh.c@gmail.com>
2020-12-14 15:17:41 -0600

    Finished problem 2

--- 2/2.py
+++ 2/2.py
@@ -1,1 +1,10 @@
-
+sum = 0
+smallerTerm = 1
+largerTerm = 1  
+while largerTerm < 4000000:
+    if(largerTerm % 2 == 0):
+        sum += largerTerm
+    next = smallerTerm + largerTerm
+    smallerTerm = largerTerm
+    largerTerm = next
+print(sum)
