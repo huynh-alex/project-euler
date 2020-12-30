d6e8249ab4ab9604071013f4a5322a2c7c269c3c
unknown <alexhuynh.c@gmail.com>
2020-12-29 18:02:43 -0600

    53/53.py

---
+++ 53/53.py
@@ -0,0 +1,20 @@
+def fac(n):
+    product = 1
+    for i in range(1, n+1):
+        product *= i
+    return product
+
+def ncr(n, r):
+    return fac(n) / ((fac(r))*fac(n-r))
+
+def ncrUpto(n):
+    vals = 0
+    for i in range(1,n+1):
+        for j in range(1, i):
+            result = ncr(i,j)
+            if result > 1000000:
+                vals+=1
+    return vals
+
+print(ncrUpto(100))
+print(ncr(23,10))
